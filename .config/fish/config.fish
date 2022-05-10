a5494acd39788c804e984e11d202134e8ccf218f
Wes <wkaminer@fastmail.com>
2022-05-10 16:18:55 -0500

    + fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# Theme
+set -g theme_color_scheme terminal-dark
+
+# Aliases
+alias vim="nvim"
+alias cls="clear"
+
+# Aliases for Exa
+if type -q exa
+    alias ll "exa -l -g --icons"
+    alias lla "ll -a"
+end
+
